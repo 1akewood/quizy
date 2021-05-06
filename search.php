d1c201e30b804628cbd7f15df289cab20b92bfe7
ohhyunmin <hoog7144@naver.com>
2021-05-06 14:23:59 +0900

    search update

--- search.php
+++ search.php
@@ -106,6 +106,9 @@ require_once "config.php";
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1.4em" width="1.4em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16 6l2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z"></path>
             </svg>
+            <?php
+            echo "'".$_POST['keyword']."' 에 대한";
+            ?>
             &nbsp;검색 결과
         </h6>
         <?php
