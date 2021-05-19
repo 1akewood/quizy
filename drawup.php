074261117732747702c35c94530d7fc5c4dbc753
ohhyunmin <hoog7144@naver.com>
2021-05-19 16:25:04 +0900

    fixed checkbox

--- drawup.php
+++ drawup.php
@@ -1,182 +1,108 @@
-<?php
-
-// Include config file
-require_once "config.php";
- 
-// Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE username = :username";
-        
-        if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-            
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                if($stmt->rowCount() == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            unset($stmt);
-        }
-    }
-    
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-    
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
-         
-        if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
-            
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Redirect to login page
-                header("location: signin.php");
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            unset($stmt);
-        }
-    }
-    
-    // Close connection
-    unset($pdo);
-}
-?>
- 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../que.ico">
-    <title>Signup for Quizy</title>
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
 
-    <!-- Bootstrap core CSS -->
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="drawup.css">
+    <title>퀴즈 만들기</title>
 
-    <!-- Custom styles for this template -->
-    <link href="signup.css" rel="stylesheet">
-    <script src="https://cdn.tiny.cloud/1/3clrscc3756qt72cqykhlctfkese9uo80wsspq64ng37449h/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
-<body>
-    <div class="wrapper">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-	    <div class="form-group">
-	    <div class="container mt-4 mb-4">
-  <div class="row justify-content-md-center">
-    <div class="col-md-12 col-lg-8">
-      <h1 class="h2 mb-4">Submit issue</h1>
-      <label>Describe the issue in detail</label>
-      <div class="form-group">
-	 <textarea id="editor" style="width:200%"></textarea>
-	 <script>
-           tinymce.init({
-              selector:'#editor',
-              menubar: false,
-              statusbar: false,
-              plugins: 'autoresize anchor autolink charmap code codesample directionality fullpage help hr image imagetools insertdatetime link lists media nonbreaking pagebreak preview print searchreplace table template textpattern toc visualblocks visualchars',
-              toolbar: 'h1 h2 bold italic strikethrough blockquote bullist numlist backcolor | link image media | removeformat help fullscreen ',
-              skin: 'bootstrap',
-              toolbar_drawer: 'floating',
-              min_height: 200,
-              autoresize_bottom_margin: 16,
-              setup: (editor) => {
-                editor.on('init', () => {
-                    editor.getContainer().style.transition="border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out"
-                });
-                editor.on('focus', () => {
-                    editor.getContainer().style.boxShadow="0 0 0 .2rem rgba(0, 123, 255, .25)",
-                    editor.getContainer().style.borderColor="#80bdff"
-                });
-                editor.on('blur', () => {
-                    editor.getContainer().style.boxShadow="",
-                    editor.getContainer().style.borderColor=""
-                });
-              }
-          });
-        </script>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+
+<body cellpadding="0" cellspacing="0" marginleft="0" margintop="0" width="100%" height="100%" align="center">
+
+<div class="card align-middle" style="width:40rem; border-radius:20px;">
+    <div class="card-title" style="margin-top:30px;">
+        <h2 class="card-title text-center" style="color:#113366;">새로운 퀴즈</h2>
+        <hr>
     </div>
-  </div>
+    <div class="card-body">
+        <input type="text" id="quiz-article-description" class="form-control" placeholder="퀴즈 제목" required autofocus><BR>
+        <input type="text" id="quiz-article-content" class="form-control" placeholder="퀴즈 설명" required><BR>
+    </div>
 </div>
-		<label>아이디</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>" placeholder="Username" required autofocus>
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>비밀번호</label>
-		<input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" placeholder="Password" required autofocus>
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+
+<div class="card align-middle" style="width:40rem; border-radius:20px;">
+    <div class="card-title" style="margin-top:30px;">
+        <h6 class="card-title text-right mr-3" style="color:#113366;">X</h6>
+    </div>
+    <div class="card-body">
+        <input type="text" id="quiz-es-description" class="form-control" placeholder="퀴즈1 제목" required autofocus><BR>
+        <input type="text" id="quiz-es-content" class="form-control d-inline" placeholder="퀴즈 정답" required>
+        <div class="btn-group">
+            <button id="quiz-es-type" type="button" class="btn btn-primary dropdown-toggle d-inline ml-3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                약술형
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">서술형</a>
+                <a class="dropdown-item" href="#">객관식</a>
+                <a class="dropdown-item" href="#">체크박스</a>
             </div>
-            <div class="form-group">
-                <label>비밀번호 확인</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>" placeholder="Confirm Password" required autofocus>
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-		<input type="submit" class="btn btn-primary" value="완료">
-                <input type="reset" class="btn btn-secondary ml-2" value="초기화">
-            </div>
-	    <hr>
-	    <p>계정이 이미 있으신가요? <a href="signin.php">로그인 하기</a></p>
-	    <p class="mt-4 mb-3 text-muted">&copy; Quizy 2021</p>
-        </form>
-    </div>   
+        </div>
+    </div>
+</div>
+
+<div class="modal">
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script>
+    $appendNum = 1;
+    $( document ).ready(function() {
+        $('.btn-group').each(function (key, dropdown) {
+            var $dropdown = $(dropdown);
+            $dropdown.find('.dropdown-menu a').on('click', function () {
+                let temp = $dropdown.find('#quiz-es-type').text().trim();
+                $(this).parent().parent().prev().remove();
+                if($(this).text().trim() === "체크박스") {
+                    $(this).parent().parent().prev().after('<div class="checkbox">' +
+                        '<input type="radio"  name="checkbox" value=""/>' +
+                        '<input type="text">'+
+                        '</div>');
+                    $appendpoint = $(this);
+                    $removepoint = document.querySelector('.checkbox').parentElement;
+                    console.log($removepoint = document.querySelector('.checkbox').parentElement);
+                    $(this).parent().parent().prev().after('<div><input type="button" value="보기 추가" class="checkbox-button" onclick="appendchoice($appendpoint)"> <input type="button" value="보기 삭제" class="checkbox-button" onclick="removechoice($removepoint)"/></div>');
+
+                }else if($(this).text().trim() === "약술형"){
+                    $(this).parent().parent().prev().after('<input type="text" id="quiz-es-awnser" class="form-control d-inline" placeholder="약술형 정답" required>');
+                }else if($(this).text().trim() === "객관식"){
+                    $(this).parent().parent().prev().after('<p>Select Number</p>');
+
+                }else{
+                    $(this).parent().parent().prev().after('<input type="text" id="quiz-es-awnser" class="form-control d-inline" placeholder="서술형 정답" required>');
+                }
+                $dropdown.find('#quiz-es-type').text($(this).text());
+                $(this).text(temp);
+            });
+        });
+    });
+
+    function appendchoice($appendpoint){
+        if($appendNum < 5) {
+            $($appendpoint).parent().parent().prev().before('<div class="checkbox">' +
+                '<input type="radio" name="checkbox" value=""/>' +
+                '<input type="text">' +
+                '</div>');
+            $appendNum++;
+        }
+    }
+    function removechoice($removepoint){
+        if($appendNum > 1 ){
+            $checkboxNode = document.querySelectorAll('.checkbox');
+            $checkboxNode[$checkboxNode.length-1].remove();
+            $appendNum--;
+        }
+    }
 </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script> 
 </body>
 </html>
